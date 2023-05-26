7c1bf28b1bb283b779fbc8d56de31048e2755fe7
yb6b <seetxx@qq.com>
2023-05-26 09:57:06 +0800

    del e symbol location.hash

--- docs/guide/symbols/ERepl.vue
+++ docs/guide/symbols/ERepl.vue
@@ -1,26 +1,12 @@
 <script setup lang="ts">
 import { ref, provide } from "vue";
 import type { Ref } from "vue";
-import { onMounted, watch } from "vue";
 import FormatTable from "./FormatTable.vue";
 import FormatKbd from "./FormatKbd.vue";
 import Data from "./ESymbols.json";
 const codes = ref("e");
 provide("code", codes);
 const kbdStyle: Ref<"a" | "l" | "r"> = ref("a");
-watch(codes, (n) => {
-  window.location.hash = n;
-  window.document.title = `〖${n}〗 e键下的符号 | 哲豆音形`;
-  codes.value = n;
-});
-onMounted(() => {
-  let oldHash = window.location.hash.slice(1);
-  if (oldHash.length === 0) {
-    oldHash = "e";
-    window.location.hash = oldHash;
-  }
-  codes.value = oldHash;
-});
 </script>
 
 <template>
--- docs/guide/symbols/FormatTable.vue
+++ docs/guide/symbols/FormatTable.vue
@@ -7,39 +7,41 @@ const atLeastOne = (n) => (n < 1 ? 1 : n);
 </script>
 
 <template>
-  <table>
-    <tr>
-      <th>编码</th>
-      <th>符号</th>
-      <th>说明</th>
-      <th>更深一层</th>
-    </tr>
-    <template v-for="a of Alphabet">
-      <template v-if="SymbolData?.[codes + a]">
-        <tr>
-          <td :rowspan="atLeastOne(SymbolData[codes + a].words.length)">
-            <code>{{ codes + a }}</code>
-          </td>
-          <td>{{ SymbolData[codes + a].words[0]?.content ?? "" }}</td>
-          <td>{{ SymbolData[codes + a].words[0]?.detail ?? "" }}</td>
-          <td :rowspan="atLeastOne(SymbolData[codes + a].words.length)">
-            <a
-              @click="
-                () => {
-                  codes = codes + a;
-                }
-              "
-              >{{ SymbolData[codes + a].title }}</a
-            >
-          </td>
-        </tr>
-        <template v-for="b of SymbolData[codes + a].words.slice(1)">
+  <div style="display: flex; justify-content: center">
+    <table>
+      <tr>
+        <th>编码</th>
+        <th>符号</th>
+        <th>说明</th>
+        <th>更深一层</th>
+      </tr>
+      <template v-for="a of Alphabet">
+        <template v-if="SymbolData?.[codes + a]">
           <tr>
-            <td>{{ b.content }}</td>
-            <td>{{ b.detail }}</td>
+            <td :rowspan="atLeastOne(SymbolData[codes + a].words.length)">
+              <code>{{ codes + a }}</code>
+            </td>
+            <td>{{ SymbolData[codes + a].words[0]?.content ?? "" }}</td>
+            <td>{{ SymbolData[codes + a].words[0]?.detail ?? "" }}</td>
+            <td :rowspan="atLeastOne(SymbolData[codes + a].words.length)">
+              <a
+                @click="
+                  () => {
+                    codes = codes + a;
+                  }
+                "
+                >{{ SymbolData[codes + a].title }}</a
+              >
+            </td>
           </tr>
+          <template v-for="b of SymbolData[codes + a].words.slice(1)">
+            <tr>
+              <td>{{ b.content }}</td>
+              <td>{{ b.detail }}</td>
+            </tr>
+          </template>
         </template>
       </template>
-    </template>
-  </table>
+    </table>
+  </div>
 </template>
