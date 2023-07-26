ff7acf90539458cd1cc36e72edc688ca1d9a382f
yb6b <seetxx@qq.com>
2023-07-26 17:26:46 +0800

    延迟显示下载进度条

--- docs/assist/Assist.vue
+++ docs/assist/Assist.vue
@@ -4,20 +4,26 @@ import { fetchMb, Db } from "./fetchMb";
 import Main from "./Main.vue";
 
 const mb: Ref<Db | undefined> = ref();
+const showProgress = ref(false);
 const downloadProgress = shallowReactive({
   max: 0,
   current: 0,
 });
 onMounted(() => {
+  setTimeout(() => {
+    showProgress.value = true;
+  }, 300);
   fetchMb(downloadProgress).then((v) => (mb.value = v));
 });
 </script>
 
 <template>
   <div style="text-align: center; color: grey" v-if="typeof mb === 'undefined'">
-    正在加载哲豆码表……
-    <br />
-    <progress :max="downloadProgress.max" :value="downloadProgress.current" />
+    <div v-show="showProgress">
+      正在加载哲豆码表……
+      <br />
+      <progress :max="downloadProgress.max" :value="downloadProgress.current" />
+    </div>
   </div>
   <Main v-else :mb="mb" />
 </template>
