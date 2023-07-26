5ef69a06b7591e21b11bd5ef687df4eae20ed9e5
yb6b <seetxx@qq.com>
2023-07-26 17:56:09 +0800

    accelerate

--- docs/assist/Assist.vue
+++ docs/assist/Assist.vue
@@ -1,24 +1,32 @@
 <script setup lang="ts">
-import { ref, onMounted, Ref, shallowReactive } from "vue";
+import { ref, onMounted, shallowReactive } from "vue";
 import { fetchMb, Db } from "./fetchMb";
 import Main from "./Main.vue";
 
-const mb: Ref<Db | undefined> = ref();
 const showProgress = ref(false);
+const done = ref(false);
+
+let mb: Db;
+
 const downloadProgress = shallowReactive({
   max: 0,
   current: 0,
 });
+
 onMounted(() => {
   setTimeout(() => {
     showProgress.value = true;
   }, 300);
-  fetchMb(downloadProgress).then((v) => (mb.value = v));
+
+  fetchMb(downloadProgress).then((v) => {
+    mb = v;
+    done.value = true;
+  });
 });
 </script>
 
 <template>
-  <div style="text-align: center; color: grey" v-if="typeof mb === 'undefined'">
+  <div style="text-align: center; color: grey" v-if="!done">
     <div v-show="showProgress">
       正在加载哲豆码表……
       <br />
