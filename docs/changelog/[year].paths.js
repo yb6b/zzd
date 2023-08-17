fd8747c3e22d2084efb7800f886ac80b9c5c8bef
yb6b <seetxx@qq.com>
2023-08-17 17:54:16 +0800

    fix changelog

--- docs/changelog/[year].paths.js
+++ docs/changelog/[year].paths.js
@@ -1,22 +1,8 @@
 import { LOGS_GROUP_YEAR, LOGS } from "./readLog";
 import { writeFileSync } from "node:fs";
-import { resolve } from "node:path";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
 
-const logData = LOGS_GROUP_YEAR;
-
-export default {
-  paths: () =>
-    logData.map((v) => ({
-      params: {
-        year: v[0].year,
-      },
-      content: formatContentInOneYear(v),
-    })),
-};
-
-/**
- * @param {@import('./readLog').EachLog[]} logsInOneYear
- */
 function formatContentInOneYear(logsInOneYear) {
   const recordInDay = logsInOneYear.map(
     (v) => `## ${v.year}年${v.month}月${v.day}日
@@ -45,7 +31,8 @@ ${content.map(encodeEachline).join("<br>\n").trimEnd()}
 
 function writeIndexMd() {
   const { year, month, day, content } = LOGS[0];
-  const data = `# 更新历史
+  const data = `
+# 更新历史
 
 哲豆音形保持着高强度的更新频率，加词、调整重码等等。2018 年以前的资料或有缺失。
 
@@ -54,8 +41,22 @@ function writeIndexMd() {
 最近一次更新在 **${year}年${month}月${day}日**：
 
 ${formatContent(content)}
+
 `;
-  writeFileSync(resolve(__dirname, "./index.md"), data);
+  writeFileSync(
+    resolve(dirname(fileURLToPath(import.meta.url)), "./index.md"),
+    data
+  );
 }
 
 writeIndexMd();
+
+export default {
+  paths: () =>
+    LOGS_GROUP_YEAR.map((v) => ({
+      params: {
+        year: v[0].year,
+      },
+      content: formatContentInOneYear(v),
+    })),
+};
